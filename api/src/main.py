9bfa57a4886bba77a8b586b71951fcfcc5c07eb4
pheni-18 <pheni.engineer@gmail.com>
2022-06-25 12:43:43 +0900

    resolve cors error

--- api/src/main.py
+++ api/src/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
 from src.presentation.controllers import router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 api = FastAPI()
+
+origins = [
+    'http://localhost:3000',
+]
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 api.include_router(router)
 
